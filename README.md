00364ea0e9c66f0e07306b434e623d49504be1be
DustinDiazLopez <hunterghost526@gmail.com>
2019-08-31 15:28:54 -0400

    Updates jar

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Combine 
-Application utilizing [JavaFX](https://en.wikipedia.org/wiki/JavaFX) and [PDFBox](https://pdfbox.apache.org/).
+Application utilizing [JavaFX](https://en.wikipedia.org/wiki/JavaFX) and [libs.PDFBox](https://pdfbox.apache.org/).
 This [drag-and-drop](https://docs.oracle.com/javafx/2/drag_drop/jfxpub-drag_drop.htm) application takes in a list of PDF files and combines/merge them into one file.
 
 ## What I learned
-- Merging PDF files with [PDFBox](https://pdfbox.apache.org/).
+- Merging PDF files with [libs.PDFBox](https://pdfbox.apache.org/).
 - Manual [JavaFX](https://en.wikipedia.org/wiki/JavaFX) styling and setup.
 - [Drag and drop](https://docs.oracle.com/javafx/2/drag_drop/jfxpub-drag_drop.htm) functionality in [JavaFX](https://en.wikipedia.org/wiki/JavaFX).
---
+++ src/CombinePDF/Convert.java
@@ -0,0 +1,24 @@
+package CombinePDF;
+
+import com.documents4j.api.DocumentType;
+import com.documents4j.api.IConverter;
+import com.documents4j.job.LocalConverter;
+
+import java.io.*;
+
+public class Convert {
+    public static void toPDF(String docPath, String pdfPath) {
+        File inputWord = new File(docPath);
+        File outputFile = new File(pdfPath);
+        try {
+            InputStream docxInputStream = new FileInputStream(inputWord);
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            IConverter converter = LocalConverter.builder().build();
+            converter.convert(docxInputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
+            outputStream.close();
+            System.out.println("\u001B[32m" + "success!" + "\u001B[0m");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
